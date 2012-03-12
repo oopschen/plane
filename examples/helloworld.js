d2574941f327167e4e11de22a3b9ebc96c61d6ff
ray <linxray@gmail.com>
2012-03-12 22:11:52 +0800

    init project

---
+++ examples/helloworld.js
@@ -0,0 +1,5 @@
+var plane = require('plane');
+plane.fly(__dirname+'/yard',function(tower){
+	tower.fire('hello','world');
+	tower.fire('new.hi','ok');
+});
---
+++ plane.js
@@ -0,0 +1,101 @@
+/**
+* auto detect dir and parse every file to reg events
+* rules:
+* 	exports.evt_click stand for event "click" , casesensitive
+* 	exports.evt_click_dot stand for event "click.dot" , casesensitive
+**/
+var fs = require('fs');
+var xtower = require('./lib/xtower');
+var gEvts = {
+	detectDir:'_detect_dir',
+	regEvt:'_reg_evt',
+	loadEvt:'_load_evt',
+	complete:'_complete_load'
+};
+
+var assemleFile = function() {
+	var fullPath = '';
+	for(var i=0;i<arguments.length;i++) {
+		fullPath += arguments[i] + '/';
+	};
+	return fullPath.replace(/\/\//g,'/').replace(/\/$/g,'');
+};
+var isRegProp = function(prop){
+	if(!prop) {
+		return false;
+	}
+	return 0 == prop.indexOf('evt_');
+};
+var formatProp = function(prop) {
+	if(!prop) {
+		return '';
+	}
+	return prop.replace(/^evt_(.*)$/,'$1').replace(/_/,'.');
+};
+/**
+* input dirname,relative to this file
+* output [files]
+*/
+var detectDir = function(path,callback,tower) {
+	fs.readdir(path,function(error,files){
+		if(error) {
+			console.error('directory not exits %s',path);
+			return;
+		}
+		if(!files || 0 == files.length) {
+			console.log('no files found at %s',path);
+			return;
+		}
+		for(var i=0;i<files.length;i++) {
+			if(!callback) {
+				continue;
+			}
+			callback(assemleFile(path,files[i]));
+		}
+	});
+};
+xtower.on(gEvts.detectDir,detectDir);
+
+var regEvt = function(name,cb,tower){
+	tower.on(name,cb);
+	tower.fire(gEvts.complete);
+};
+xtower.on(gEvts.regEvt,regEvt);
+
+/**
+* parse every file and reg every method to tower
+*/
+var loadEvt = function(file,tower){
+	console.log('load file success[%s]',file);
+	var obj = require(file);
+	for(var i in obj) {
+		if(!isRegProp(i)) {
+			continue;
+		}
+		tower.fire(gEvts.regEvt,formatProp(i),obj[i]);
+	}
+};
+xtower.on(gEvts.loadEvt,loadEvt);
+
+
+exports.fly = function(path,cb){
+	xtower.on(gEvts.complete,cb);
+	var initDetect = function(file) {
+		if(!file) {
+			console.error("[%s] detect error",file);
+			return;
+		}
+		fs.stat(file,function(error,stats) {
+			if(error) {
+				return;
+			}
+			// if file fire loadEvt
+			if(stats.isFile()) {
+				xtower.fire(gEvts.loadEvt,file);
+			} else if(stats.isDirectory()) {
+				xtower.fire(gEvts.detectDir,file,initDetect);
+			}
+		});
+	};
+	xtower.fire(gEvts.detectDir,assemleFile(path),initDetect);
+};
